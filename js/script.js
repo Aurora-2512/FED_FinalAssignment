994ddace7eefa24ee637ddecae3c829c409e21b2
akshaya bandi <s10268087@connect.np.edu.sg>
2025-02-03 11:49:13 +0800

    Image Addition

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 
 const listings = [
     { image: "image/smartphone.jpeg", category: "Electronics", title: "Samsung Galaxy A52", price: "$250" },
-    { image: "image/dji_osmo_pocket.jpg", category: "Electronics", title: "Garden Tools", price: "$50" },
-    { image: "image/gray_cargopants.jpg", category: "Fashion", title: "Designer Jacket", price: "$100" },
-    { image: "image/jbl_headphone.jpg", category: "Electronics", title: "Bicycle", price: "$200" },
-    { image: "image/white_stripe_trousers.jpg",category: "Vehicles", title: "Bicycle", price: "$200" },
-    { image: "image/Sony_headphones.jpg", category: "Electronics", title: "Sony Headphones"},
+    { image: "image/dji_osmo_pocket.jpg", category: "Electronics", title: "Pocket Camera", price: "$150" },
+    { image: "image/denim_jacket.jpg", category: "Fashion", title: "Designer Jacket", price: "$270" },
+    { image: "image/bicycle.jpg", category: "Vehicles", title: "Bicycle", price: "$400" },
+    { image: "image/white_stripe_trousers.jpg",category: "Fashion", title: "Trousers", price: "$65" },
+    { image: "image/Sony_headphones.jpg", category: "Electronics", title: "Sony Headphones", price: "$350"},
     { image: "image/smartphone.jpeg", category: "Electronics", title: "Samsung Galaxy A52", price: "$250" },
-    { image: "image/dji_osmo_pocket.jpg",category: "Electronics", title: "Garden Tools", price: "$50" },
-    { image: "image/gray_cargopants.jpg", category: "Fashion", title: "Designer Jacket", price: "$100" },
-    { image: "image/jbl_headphone.jpg", category: "Electronics", title: "Bicycle", price: "$200" },
-    { image: "image/white_stripe_trousers.jpg", category: "Vehicles", title: "Bicycle", price: "$200" },
-    { image: "image/Sony_headphones.jpg", category: "Electronics", title: "Sony Headphones"}
-
+    { image: "iimage/gardening_tool.jpg",category: "Home & Garden", title: "Garden Tools", price: "$50" },
+    { image: "image/gray_cargopants.jpg", category: "Fashion", title: "Cargo Pants", price: "$50" },
+    { image: "image/jbl_headphone.jpg", category: "Vehicles", title: "JBL Headphones", price: "$300" },
+    { image: "image/scooter.jpg", category: "Vehicles", title: "Scooter", price: "$200" },
+    { image: "image/soil.jpg", category: "Home & Garden", title: "Potting Soil", price: "$40"}
+    { image: "image/bracelet.jpg", category: "Fashion", title: "Vintage Bracelet", price: "$250"}
+    { image: "image/hanging_pots.jpg", category: "Home & Garden", title: "Indoor Hanging Pots", price: "$35"}
+    { image: "image/airpods.jpg", category: "Electronics", title: "Airpods Pro 2", price: "$450"}
 ];
 //looping
 const featuredListingsContainer = document.getElementById("featuredListings");
